ff13955fba9b1372c411d3c92bb5565e9331be47
Rajasekar J <kriya0784@gmail.com>
2014-03-26 12:11:42 +0530

    Added dashboard ejs file for dashboard.

--- express_now/app.js
+++ express_now/app.js
@@ -25,6 +25,7 @@ app.use(app.router);
 
 app.get('/', routes.index);
 app.get('/users', users.list);
+app.get('/dashboard', routes.dashboard);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- express_now/routes/index.js
+++ express_now/routes/index.js
@@ -2,3 +2,9 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+/* GET dashboard page. */
+exports.dashboard = function(req, res){
+  res.render('dashboard', { title: 'Dashboard' });
+};
+
