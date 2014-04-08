2bdd263695262d9179a9117c03100b4da7f25423
Rajasekar J <kriya0784@gmail.com>
2014-04-08 22:30:45 +0530

    Added new files dashboard

--- express_now/routes/index.js
+++ express_now/routes/index.js
@@ -1,10 +1,14 @@
 /* GET home page. */
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Express' });
+// };
+
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
 /* GET dashboard page. */
 exports.dashboard = function(req, res){
-  res.render('dashboard', { title: 'Dashboard' });
+  res.render('dashboard', { title: 'Dashboard', heading: 'Model' });
 };
 
